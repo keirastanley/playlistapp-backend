c0719a722dd79184fbc4d7eda59ad0b82f1bdc1a
keirastanley <keiraestanley@gmail.com>
2023-02-24 18:31:48 +0000

    Route and service to remove a comment added

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,93 +1,101 @@
 import express from "express";
-import { getAllPlaylists, getPlaylistById, addNewPlaylist, addTrackToPlaylist, deleteTrackFromPlaylist, editPlaylistDetails, deletePlaylist, addComment, removeAccessUser, addAccessUser} from "../services/playlists.js";
+import { getAllPlaylists, getPlaylistById, addNewPlaylist, addTrackToPlaylist, deleteTrackFromPlaylist, editPlaylistDetails, deletePlaylist, addComment, removeAccessUser, addAccessUser, removeComment } from "../services/playlists.js";
 
 const playlistsRouter = express.Router();
 
 playlistsRouter.get("/", async (req, res) => {
-    try {
-      const playlists = await getAllPlaylists();
-      res.send({ success: true, payload: playlists });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+  try {
+    const playlists = await getAllPlaylists();
+    res.send({ success: true, payload: playlists });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 playlistsRouter.get("/:id", async (req, res) => {
-    try { 
-        const playlists = await getPlaylistById(req.params.id);
-        res.send({ success: true, payload: playlists });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const playlists = await getPlaylistById(req.params.id);
+    res.send({ success: true, payload: playlists });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 playlistsRouter.post("/", async (req, res) => {
-    try { 
-        const result = await addNewPlaylist(req.body);
-        res.send({ success: true, payload: result });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const result = await addNewPlaylist(req.body);
+    res.send({ success: true, payload: result });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 playlistsRouter.patch("/:id", async (req, res) => {
-    if (req.query.action === "add-track") {
-        try { 
-            const result = await addTrackToPlaylist(req.params.id, req.body);
-            res.send({ success: true, payload: result });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  if (req.query.action === "add-track") {
+    try {
+      const result = await addTrackToPlaylist(req.params.id, req.body);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    if (req.query.action === "delete-track") {
-        try { 
-            const result = await deleteTrackFromPlaylist(req.params.id, req.body);
-            res.send({ success: true, payload: result });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  }
+  if (req.query.action === "delete-track") {
+    try {
+      const result = await deleteTrackFromPlaylist(req.params.id, req.body);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    if (req.query.action === "update-playlist") {
-        try { 
-            const result = await editPlaylistDetails(req.params.id, req.body);
-            res.send({ success: true, payload: result });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  }
+  if (req.query.action === "update-playlist") {
+    try {
+      const result = await editPlaylistDetails(req.params.id, req.body);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    if (req.query.action === "add-comment") {
-        try { 
-            const result = await addComment(req.params.id, req.body[0].track_id, req.body[1]);
-            res.send({ success: true, payload: result });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  }
+  if (req.query.action === "add-comment") {
+    try {
+      const result = await addComment(req.params.id, req.body[0].track_id, req.body[1]);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    if (req.query.action === "add-access") {
-        try { 
-            const result = await addAccessUser(req.params.id, req.body.user_id);
-            res.send({ success: true, payload: result });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  }
+  if (req.query.action === "remove-comment") {
+    try {
+      const result = await removeComment(req.params.id, req.body.track_id, req.body.comment_id);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    if (req.query.action === "remove-access") {
-        try {
-            const result = await removeAccessUser(req.params.id, req.body.user_id);
-            res.send({ success: true, payload: result });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  }
+  if (req.query.action === "add-access") {
+    try {
+      const result = await addAccessUser(req.params.id, req.body.user_id);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
+  }
+  if (req.query.action === "remove-access") {
+    try {
+      const result = await removeAccessUser(req.params.id, req.body.user_id);
+      res.send({ success: true, payload: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
 })
 
 playlistsRouter.delete("/:id", async (req, res) => {
-    try { 
-        const result = await deletePlaylist(req.params.id);
-        res.send({ success: true, payload: result });
-        } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const result = await deletePlaylist(req.params.id);
+    res.send({ success: true, payload: result });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 export default playlistsRouter;
--- services/playlists.js
+++ services/playlists.js
@@ -2,74 +2,85 @@ import { playlistsData } from "../app.js";
 import { ObjectId } from "mongodb";
 
 export async function getAllPlaylists() {
-    const data = await playlistsData.find({}).toArray();
-    return data;
+  const data = await playlistsData.find({}).toArray();
+  return data;
 }
 
 export async function getPlaylistById(id) {
-    const playlist = await playlistsData.find({ _id: new ObjectId(`${id}`) }).toArray();
-    return playlist;
+  const playlist = await playlistsData.find({ _id: new ObjectId(`${id}`) }).toArray();
+  return playlist;
 }
 
 export async function addNewPlaylist(playlist) {
-    await playlistsData.insertOne(playlist)
-    return playlist;
+  await playlistsData.insertOne(playlist)
+  return playlist;
 }
 
 export async function addTrackToPlaylist(id, track) {
-    const update = { $addToSet: { tracks: track } }
-    await playlistsData.updateOne({ _id: new ObjectId(`${id}`) }, update)
-    const editedPlaylist = await playlistsData.find({
-        _id: new ObjectId(`${id}`)
-    }).toArray();
-    return editedPlaylist;
+  const update = { $addToSet: { tracks: track } }
+  await playlistsData.updateOne({ _id: new ObjectId(`${id}`) }, update)
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${id}`)
+  }).toArray();
+  return editedPlaylist;
 }
 
 export async function addComment(playlist_id, track_id, comment) {
-    const test = await playlistsData.findOneAndUpdate({"tracks.id": track_id}, { $addToSet: { "tracks.$.comments": comment }})
-    const editedPlaylist = await playlistsData.find({
-        _id: new ObjectId(`${playlist_id}`)
-    }).toArray();
-    return editedPlaylist;
+  const test = await playlistsData.findOneAndUpdate({ "tracks.id": track_id }, { $addToSet: { "tracks.$.comments": comment } })
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${playlist_id}`)
+  }).toArray();
+  return editedPlaylist;
+}
+
+export async function removeComment(playlist_id, track_id, comment_id) {
+  await playlistsData.findOneAndUpdate(
+    { 'tracks.id': track_id },
+    { $pull: { 'tracks.$.comments': { id: comment_id } } }
+  )
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${playlist_id}`)
+  }).toArray();
+  return editedPlaylist;
 }
 
 export async function deleteTrackFromPlaylist(playlist_id, track_id) {
-    const update = { $pull: { "tracks": track_id } }
-    await playlistsData.updateOne({ _id: new ObjectId(`${playlist_id}`) }, update)
-    const editedPlaylist = await playlistsData.find({
-        _id: new ObjectId(`${playlist_id}`)
-    }).toArray();
-    return editedPlaylist;
+  const update = { $pull: { "tracks": track_id } }
+  await playlistsData.updateOne({ _id: new ObjectId(`${playlist_id}`) }, update)
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${playlist_id}`)
+  }).toArray();
+  return editedPlaylist;
 }
 
 export async function addAccessUser(playlist_id, user_id) {
-    const update = { $addToSet: { "access": user_id } }
-    await playlistsData.updateOne({ _id: new ObjectId(`${playlist_id}`) }, update)
-    const editedPlaylist = await playlistsData.find({
-        _id: new ObjectId(`${playlist_id}`)
-    }).toArray();
-    return editedPlaylist;
+  const update = { $addToSet: { "access": user_id } }
+  await playlistsData.updateOne({ _id: new ObjectId(`${playlist_id}`) }, update)
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${playlist_id}`)
+  }).toArray();
+  return editedPlaylist;
 }
 
 export async function removeAccessUser(playlist_id, user_id) {
-    const update = { $pull: { "access": user_id } }
-    await playlistsData.updateOne({ _id: new ObjectId(`${playlist_id}`) }, update)
-    const editedPlaylist = await playlistsData.find({
-        _id: new ObjectId(`${playlist_id}`)
-    }).toArray();
-    return editedPlaylist;
+  const update = { $pull: { "access": user_id } }
+  await playlistsData.updateOne({ _id: new ObjectId(`${playlist_id}`) }, update)
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${playlist_id}`)
+  }).toArray();
+  return editedPlaylist;
 }
 
 export async function editPlaylistDetails(id, update) {
-    const playlistUpdate = { $set: update }
-    await playlistsData.updateOne({ _id: new ObjectId(`${id}`) }, playlistUpdate)
-    const editedPlaylist = await playlistsData.find({
-        _id: new ObjectId(`${id}`)
-    }).toArray();
-    return editedPlaylist;
+  const playlistUpdate = { $set: update }
+  await playlistsData.updateOne({ _id: new ObjectId(`${id}`) }, playlistUpdate)
+  const editedPlaylist = await playlistsData.find({
+    _id: new ObjectId(`${id}`)
+  }).toArray();
+  return editedPlaylist;
 }
 
 export async function deletePlaylist(id) {
-    await playlistsData.deleteOne({ _id: new ObjectId(`${id}`) })
-    return `Playlist with id ${id} deleted`;
+  await playlistsData.deleteOne({ _id: new ObjectId(`${id}`) })
+  return `Playlist with id ${id} deleted`;
 }
